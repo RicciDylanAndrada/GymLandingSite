894c7d9e8a2156fa0574171240e536374f058731
Ricci <riccidandrada@gmail.com>
2021-08-24 22:12:13 -0400

    rearranged sections

--- src/component/Body.js
+++ src/component/Body.js
@@ -12,6 +12,22 @@ import Image from 'react-bootstrap/Image'
 import largeImage from "../images/fake.jpg"
 
 
+const aboutuS =
+
+"Shore Strength  is your new home in boutique wellness."+
+ "We join the dependable standards of Pilates with forefront development to give you the most out of each exercise."+
+ " Our specialists are very prepared and fit the bill to furnish you with shape centered alterations that will push you as far as possible without bargaining your security."+
+  " Expect high-vitality quick paced exercises that leave each muscle shaking. "+
+   "Shore Strength was established in 2012 by Jeanette and Ryan and now we have 2 NYC areas… with a third in transit!"+
+
+"The Shore Strength Former was structured solely for Shore Strength to consolidate two of the best bits of Pilates hardware, the reformer and the Pilates seat. Co-structured with the Villency Design Group,"+
+ "the Shore StrengthFormer displays an extraordinary cross breed of seat and reformer Pilates techniques, mixing quality preparing and cardio for a full-body exercise."+
+
+"You can anticipate brisk, consistent changes, a quiet carriage and additional choices for shifting dimensions of protection from make the moves all the more difficult, at last focusing on the muscles all the more profoundly for a more prominent consume."+
+
+"Our Shore Strength IR class is warmed by cutting edge full range infrared boards. Full range infrared warmth is the most vitality productive approach to warm a space and you get a noteworthy increase in advantages just from going into the room."+
+
+"You’ll get a vibe decent surge of serotonin and dopamine simply like from the sun’s characteristic beams in addition to IR has been appeared to help with cell fix, flow, detoxification, and stress decrease. IR warm is the following stage in exercise advancement."
 
 
 const Body=() =>{
@@ -37,6 +53,34 @@ const Body=() =>{
 <h1>Hello</h1>
         </section>
        
+        <section id="About" class>
+<div className="wrapper">
+    <div  id="skew2" className="cus-div">
+    
+        <div className = "content">
+        <div className="AboutUs">
+        <div >
+        <h1>ABOUT US</h1>
+
+        </div>
+        <div className='AB'>
+        <h2>Shore Strength</h2>
+        <p>{aboutuS}</p>
+
+        </div>
+        </div>
+
+        
+       
+        </div>
+    </div>
+
+
+</div>
+          
+
+
+</section>   
 
 <section id="Price" class>
 <div className="wrapper">
@@ -66,27 +110,12 @@ const Body=() =>{
 </div>
 </section>
 
-
-<section id="About" class>
-<div className="wrapper">
-    <div  id="skew2" className="cus-div">
-        <div className = "content">
-        <div className="AboutUs">
-        <h1>ABOUT US</h1>
-
-        </div>
-
-        
-       
-        </div>
-    </div>
-
-
-</div>
-          
-
+<section id="Products">
 
 </section>
+
+
+
 
 
 
